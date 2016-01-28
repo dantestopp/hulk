26b6e1bba80278de564874a1dcd13a6b37f55859
happyoniens <dante.stoppini@gmail.com>
2016-01-28 21:17:20 +0100

    Start of hulk class

---
+++ src/Hulk.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * @author happyonies
+ */
+class Hulk{
+
+    /**
+    * Handles calls to static methods.
+    *
+    * @param string $name Method name
+    * @param array $params Method parameters
+    * @return mixed Callback results
+    */
+    public static function __callStatic($name, $params){
+
+    }
+
+}
