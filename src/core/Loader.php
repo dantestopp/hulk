03f0258906c6d79de6a24c71fbf5cf4af370d91e
gabriellovric <lovric.gabriel@gmail.com>
2016-01-28 22:41:52 +0100

    Added Loader
    Added placeholder functions for the loader class

---
+++ src/core/Loader.php
@@ -0,0 +1,69 @@
+<?php
+namespace Hulk\core;
+
+/**
+ * @author gabriellovric
+ */
+class Loader {
+
+    /**
+     * Registered classes.
+     *
+     * @var array
+     */
+    protected $classes = array();
+
+    /**
+     * Class instances.
+     *
+     * @var array
+     */
+    protected $instances = array();
+    
+    /**
+     * Autoload directories.
+     *
+     * @var array
+     */
+    protected static $dirs = array();
+
+    /**
+     * Register a class
+     *
+     * @param
+     * @return
+     */
+    public function register();
+    
+    /**
+     * Unregister a class
+     *
+     * @param
+     * @return
+     */
+    public function unregister();
+
+    /**
+     * Get instance of class if doesnt exist create new one
+     *
+     * @param
+     * @return
+     */
+    public function getInstance();
+
+    /**
+     * Enables or disables autoloding
+     *
+     * @param
+     * @return
+     */
+    public function autoload();
+
+    /**
+     * Used by autoloader to load classes.
+     *
+     * @param
+     * @return
+     */
+    public function load();
+}
