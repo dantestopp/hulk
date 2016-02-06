6089839750fb61673f6935907e10aca9fdea0748
happyoniens <dante.stoppini@gmail.com>
2016-02-06 11:23:05 +0100

    Added store vars in framework

--- src/core/Heart.php
+++ src/core/Heart.php
@@ -6,8 +6,36 @@ namespace Hulk\Core;
  */
 class Heart{
 
+    private $vars = [];
+
     public static function smash(){
-        
+
     }
 
+    /**
+     * Sets a variable to save in the framework
+     * @param String $key   Key
+     * @param Mixed $value value
+     */
+    public function set($key, $value){
+        $this->vars[$key] = $value;
+    }
+
+    /**
+     * Gets a saved variable
+     * @param  String $key Key
+     * @return Mixed      Saved value
+     */
+    public function get($key){
+        return $this->vars[$key];
+    }
+
+    /**
+     * Checks if a variable is exists
+     * @param  String  $key Key
+     * @return boolean      
+     */
+    public function has($key){
+        return isset($this->vars[$key]);
+    }
 }
